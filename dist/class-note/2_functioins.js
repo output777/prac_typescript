08a0c65471a8c2990e50f5a47ed74bd07f6a96e9
oh <outputor777@gmail.com>
2022-08-28 12:41:39 +0900

    타입스크립트 함수 타입

--- dist/class-note/2_functioins.js
+++ dist/class-note/2_functioins.js
@@ -1,1 +1,20 @@
 "use strict";
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a, b) {
+    return a + b;
+}
+sum(10, 20);
+// 함수의 반환 값에 타입을 정의하는 방식
+function add() {
+    return 10;
+}
+// 함수에 타입을 정의하는 방식 - 기본
+function sum2(a, b) {
+    return a + b;
+}
+// sum2(10, 20, 30, 40);   // 타입스크립트는 자바스크립트보다 엄격하게 체크해서 인자 개수가 더 많기 때문에 에러가 발생한다.
+// 함수의 옵셔널 파라미터
+function log(a, b) {
+}
+log('hellow world');
+log('hellow ts', 'abc');
---
+++ src/class-note/2_functioins.ts
@@ -0,0 +1,30 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+  return a + b;
+}
+sum(10, 20)
+
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+  return 10;
+}
+
+
+// 함수에 타입을 정의하는 방식 - 기본
+function sum2(a: number, b: number): number {
+  return a + b;
+}
+// sum2(10, 20, 30, 40);   // 타입스크립트는 자바스크립트보다 엄격하게 체크해서 인자 개수가 더 많기 때문에 에러가 발생한다.
+
+
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {
+
+}
+log('hellow world');
+log('hellow ts', 'abc');
+
+
+
